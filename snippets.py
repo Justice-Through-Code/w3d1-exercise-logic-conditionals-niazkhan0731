41946437d93f37c2362c3ab04d3f8ee3675a8ee7
Niaz Khan <niazkhan0731@gmail.com>
2023-09-12 00:30:44 -0400

    Successfully passed the unit test.

--- snippets.py
+++ snippets.py
@@ -8,27 +8,27 @@ def snippet_1():
     u = 5
     v = 2
 
-    if u * v = 10:
+    if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
-
+#snippet_1()
 
 def snippet_2():
     x = 15
     y = 25
-
+    
     z = 30
 
     if z < x:
         print("z is less than x")
 
-    elif z > x and z < y
+    elif z > x and z < y:
         print("z is between x and y")
 
-    else
+    else:
         print("z is greater than y")
-
+#snippet_2()
 
 def snippet_3():
     # TODO: Modify the comparison operator below so the `assert` statement passes
@@ -36,20 +36,21 @@ def snippet_3():
 
     a = 1
     b = 1
-    c = (a > b)
+    c = (a == b)
 
-    print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
+    print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
-
+#snippet_3()
 
 def snippet_4():
     # TODO: Modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-    d = (5 < 7) or not (8 < 20)
+    d = (5 < 7) and not (8 < 20)
 
     # TODO: Explain how d is set to False in a comment
+    # d evaluates to false by changing the or operator to and
     assert(d == False)  # <-- DO NOT EDIT THIS LINE
-
+#snippet_4()
 
 def snippet_5():
     # TODO: Modify the comparison operator so o evalutes to true
@@ -58,7 +59,8 @@ def snippet_5():
     m = "GOAT"
     n = "goat"
 
-    o = (m == n)
+    o = (m != n)
 
-    print (f"The value of o ({o}) is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
+#snippet_5()
